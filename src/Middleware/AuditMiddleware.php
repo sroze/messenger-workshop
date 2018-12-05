63213c3ebfef694f20cde456cf0663c7e210844e
Samuel ROZE <samuel.roze@gmail.com>
2018-12-05 16:28:16 +0000

    Add our IdentifierStamp

--- src/Middleware/AuditMiddleware.php
+++ src/Middleware/AuditMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Middleware;
 
+use App\Stamp\IdentifierStamp;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
@@ -13,22 +14,29 @@ class AuditMiddleware implements MiddlewareInterface
 {
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        $id = uniqid();
+        if (null === $envelope->last(IdentifierStamp::class)) {
+            $envelope = $envelope->with(new IdentifierStamp());
+            $envelope = $envelope->with(new IdentifierStamp());
+        }
+
+        $id = $envelope->last(IdentifierStamp::class)->id;
+
+        $color = substr($id, -6);
 
         if (null !== $envelope->last(ReceivedStamp::class)) {
-            echo "[$id] Received " . get_class($envelope->getMessage()) . "<br>\n";
+            echo "[$color] Received " . get_class($envelope->getMessage()) . PHP_EOL;
         } else {
-            echo "[$id] Dispatching " . get_class($envelope->getMessage()) . "<br>\n";
+            echo "<span style='background-color:#$color'>[$color] Dispatching " . get_class($envelope->getMessage()) . "</span><br>\n";
         }
 
         $envelope = $stack->next()->handle($envelope, $stack);
 
         if (null !== $envelope->last(SentStamp::class)) {
-            echo "[$id] Sent " . get_class($envelope->getMessage()) . "<br>\n";
+            echo "<span style='background-color:#$color'>[$color] Sent " . get_class($envelope->getMessage()) . "</span><br>\n";
         } else if (null !== $envelope->last(HandledStamp::class)) {
-            echo "[$id] Handled " . get_class($envelope->getMessage()) . "<br>\n";
+            echo "<span style='background-color:#$color'>[$color] Handled " . get_class($envelope->getMessage()) . "</span><br>\n";
         } else {
-            echo "[$id] I don't know ".get_class($envelope->getMessage())."<br>\n";
+            echo "<span style='background-color:#$color'>[$color] I don't know ".get_class($envelope->getMessage())."</span><br>\n";
         }
 
         return $envelope;
